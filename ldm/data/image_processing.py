195150fda25b3033a6a1e8703bcb536131821be5
trangle1302 <lengochaitrang@gmail.com>
2024-02-26 15:07:41 -0800

    add read_raw_hpa_image

--- ldm/data/image_processing.py
+++ ldm/data/image_processing.py
@@ -260,8 +260,27 @@ def load_raw_fucci_image(image_id, chs, rescale=True):
         full_res_image = (full_res_image/256).astype(np.uint8)
     assert is_between_0_255(full_res_image)
     return full_res_image
+    
 
-def load_intensity_rescaled_image_from_folder(image_id, chs):
-    full_res_image = load_raw_fucci_image(image_id, chs)
+def load_raw_hpa_image(image_id, chs, rescale=True):
+    data_dir = "/scratch/groups/emmalu/HPA_temp"
+    ch_map = {0: '_blue.png', 1: '_red.png', 2: '_green.png', 3: '_yellow.png'}
+    
+    imgs = []
+    for ch in chs:
+        if ch == "None":
+            imgarray = np.zeros((2048,2048), dtype=np.uint8)
+        else:
+            img = Image.open(f'{data_dir}/{plate_id}/{image_id}{ch_map[ch]}')
+            imgarray = np.array(img)
+        imgs.append(imgarray)
+        
+    full_res_image = np.stack(imgs, axis=-1)
+    if rescale:
+        p0, p99 = np.percentile(full_res_image, (0, 99.9))
+        full_res_image = exposure.rescale_intensity(full_res_image, in_range=(p0, p99), out_range=(0, 255)).astype(np.uint8)
+    else:
+        full_res_image = (full_res_image/256).astype(np.uint8)
     assert is_between_0_255(full_res_image)
     return full_res_image
+
