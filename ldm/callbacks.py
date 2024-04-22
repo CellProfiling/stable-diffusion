0fe56a8b1dbcf26d24762b4e4fc1f4647a744bd8
trangle1302 <lengochaitrang@gmail.com>
2024-04-22 10:21:48 -0700

    cleanup, black

--- ldm/callbacks.py
+++ ldm/callbacks.py
@@ -17,7 +17,6 @@ import torchvision
 import wandb
 
 from ldm.evaluation import metrics3
-from ldm.util import send_message_to_slack, send_image_to_slack
 
 
 class SetupCallback(Callback):
@@ -45,18 +44,27 @@ class SetupCallback(Callback):
             os.makedirs(self.cfgdir, exist_ok=True)
 
             if "callbacks" in self.lightning_config:
-                if 'metrics_over_trainsteps_checkpoint' in self.lightning_config['callbacks']:
-                    os.makedirs(os.path.join(self.ckptdir, 'trainstep_checkpoints'), exist_ok=True)
+                if (
+                    "metrics_over_trainsteps_checkpoint"
+                    in self.lightning_config["callbacks"]
+                ):
+                    os.makedirs(
+                        os.path.join(self.ckptdir, "trainstep_checkpoints"),
+                        exist_ok=True,
+                    )
             print("Project config")
             print(OmegaConf.to_yaml(self.config))
-            OmegaConf.save(self.config,
-                           os.path.join(self.cfgdir, "{}-project.yaml".format(self.now)))
+            OmegaConf.save(
+                self.config,
+                os.path.join(self.cfgdir, "{}-project.yaml".format(self.now)),
+            )
 
             print("Lightning config")
             print(OmegaConf.to_yaml(self.lightning_config))
-            OmegaConf.save(OmegaConf.create({"lightning": self.lightning_config}),
-                           os.path.join(self.cfgdir, "{}-lightning.yaml".format(self.now)))
-
+            OmegaConf.save(
+                OmegaConf.create({"lightning": self.lightning_config}),
+                os.path.join(self.cfgdir, "{}-lightning.yaml".format(self.now)),
+            )
         else:
             # ModelCheckpoint callback created log directory --- remove it
             if not self.resume and os.path.exists(self.logdir):
@@ -70,9 +78,22 @@ class SetupCallback(Callback):
 
 
 class ImageLogger(Callback):
-    def __init__(self, batch_frequency, max_images, clamp=True, increase_log_steps=True,
-                 rescale=True, disabled=False, log_on_batch_idx=False, log_first_step=False,
-                 log_to_slack=None, log_images_kwargs=None, log_validation=False, val_batch_frequency=1, monitor_val_metric=None):
+    def __init__(
+        self,
+        batch_frequency,
+        max_images,
+        clamp=True,
+        increase_log_steps=True,
+        rescale=True,
+        disabled=False,
+        log_on_batch_idx=False,
+        log_first_step=False,
+        log_to_slack=None,
+        log_images_kwargs=None,
+        log_validation=False,
+        val_batch_frequency=1,
+        monitor_val_metric=None,
+    ):
         print(f"Process {os.getpid()} in __init__()")
         super().__init__()
         self.rescale = rescale
@@ -96,21 +117,23 @@ class ImageLogger(Callback):
         self._last_val_loss = None
         self.monitor_val_metric = monitor_val_metric
         self.metrics = {"train": defaultdict(list), "val": defaultdict(list)}
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
         self.image_evaluator = metrics3.ImageEvaluator(device=device)
 
-    def _testtube(self, pl_module, images, samples, targets, refs, gt_locations, batch_idx, masks, bbox_labels, split):
+    def _testtube(self, pl_module, images, samples, targets, refs, split):
         for k in images:
             grid = torchvision.utils.make_grid(images[k])
             grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
 
             tag = f"{split}/{k}"
             pl_module.logger.experiment.add_image(
-                tag, grid,
-                global_step=pl_module.global_step)
+                tag, grid, global_step=pl_module.global_step
+            )
 
     # @rank_zero_only
-    def _wandb(self, pl_module, images, samples, targets, refs, gt_locations, bbox_coords, masks, bbox_labels, batch_idx, split):
+    def _wandb(self, pl_module, images, samples, targets, refs, batch_idx, split):
         print(f"Process {os.getpid()} in _wandb()")
         for k in images:
             grid = images[k]
@@ -119,7 +142,9 @@ class ImageLogger(Callback):
             tag = f"{split}/batch{batch_idx}_{k}"
             wandb.log({tag: image}, step=pl_module.global_step)
         print(samples.shape, targets.shape, refs.shape)
-        mse, ssim, mae, pcc, edist, cdist, iou = self.image_evaluator.calc_metrics(samples, targets)
+        mse, ssim, mae, pcc, edist, cdist, iou = self.image_evaluator.calc_metrics(
+            samples, targets
+        )
         self.metrics[split]["mse"].append(mse)
         self.metrics[split]["ssim"].append(ssim)
         self.metrics[split]["mae"].append(mae)
@@ -129,26 +154,24 @@ class ImageLogger(Callback):
         self.metrics[split]["iou"].append(iou)
 
     # @rank_zero_only
-    def log_local(self, save_dir, split, images,
-                  global_step, current_epoch, batch_idx):
+    def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
         root = os.path.join(save_dir, "images", split)
         for k in images:
             grid = images[k]
             filename = "{}_gs-{:06}_e-{:06}_b-{:06}.png".format(
-                k,
-                global_step,
-                current_epoch,
-                batch_idx)
+                k, global_step, current_epoch, batch_idx
+            )
             path = os.path.join(root, filename)
             os.makedirs(os.path.split(path)[0], exist_ok=True)
             Image.fromarray(grid).save(path)
-            # Only send validation images to slack
-            if split == "val" and self.log_to_slack and k in self.log_to_slack:
-                send_image_to_slack(f"A new image is generated by the diffusion model (group: {split}): ", path)
 
     def log_img(self, pl_module, batch, batch_idx, split="train"):
-        assert split in ['train', 'val']
-        if hasattr(pl_module, "gen_images") and callable(pl_module.gen_images) and self.max_images > 0:
+        assert split in ["train", "val"]
+        if (
+            hasattr(pl_module, "gen_images")
+            and callable(pl_module.gen_images)
+            and self.max_images > 0
+        ):
             logger = type(pl_module.logger)
 
             is_train = pl_module.training
@@ -156,7 +179,9 @@ class ImageLogger(Callback):
                 pl_module.eval()
 
             with torch.no_grad():
-                images, targets, samples = pl_module.gen_images(batch, split=split, **self.log_images_kwargs)
+                images, targets, samples = pl_module.gen_images(
+                    batch, split=split, **self.log_images_kwargs
+                )
 
             for k in images:
                 N = min(images[k].shape[0], self.max_images)
@@ -164,8 +189,10 @@ class ImageLogger(Callback):
                 if isinstance(images[k], torch.Tensor):
                     images[k] = images[k].detach().cpu()
                     if self.clamp:
-                        images[k] = torch.clamp(images[k], -1., 1.)
-                grid = torchvision.utils.make_grid(images[k], nrow=4) # nrow actually means number of columns
+                        images[k] = torch.clamp(images[k], -1.0, 1.0)
+                grid = torchvision.utils.make_grid(
+                    images[k], nrow=4
+                )  # nrow actually means number of columns
                 if self.rescale:
                     grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
                 grid = grid.transpose(0, 1).transpose(1, 2).squeeze(-1)
@@ -173,19 +200,38 @@ class ImageLogger(Callback):
                 grid = (grid * 255).astype(np.uint8)
                 images[k] = grid
 
-            self.log_local(pl_module.logger.save_dir, split, images,
-                           pl_module.global_step, pl_module.current_epoch, batch_idx)
+            self.log_local(
+                pl_module.logger.save_dir,
+                split,
+                images,
+                pl_module.global_step,
+                pl_module.current_epoch,
+                batch_idx,
+            )
 
-            logger_log_images = self.logger_log_images.get(logger, lambda *args, **kwargs: None)
-            #gt_locations, bbox_coords, masks, bbox_labels = batch["matched_location_classes"], batch["bbox_coords"], batch["mask"], batch["bbox_label"]
+            logger_log_images = self.logger_log_images.get(
+                logger, lambda *args, **kwargs: None
+            )
             refs = torch.permute(batch["ref-image"], (0, 3, 1, 2))
-            logger_log_images(pl_module, images, samples, targets, refs, [], [], [], [], batch_idx, split)
-            
+            logger_log_images(
+                pl_module,
+                images,
+                samples,
+                targets,
+                refs,
+                [],
+                [],
+                [],
+                [],
+                batch_idx,
+                split,
+            )
+
             if is_train:
                 pl_module.train()
 
     def check_frequency(self, check_idx):
-        if ((check_idx % self.batch_freq) == 0 or (check_idx in self.log_steps)):
+        if (check_idx % self.batch_freq) == 0 or (check_idx in self.log_steps):
             try:
                 self.log_steps.pop(0)
             except IndexError as e:
@@ -195,48 +241,88 @@ class ImageLogger(Callback):
         return False
 
     @rank_zero_only
-    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+    def on_train_batch_end(
+        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
+    ):
         # print(f"Process {os.getpid()} in on_train_batch_end(), batch_idx {batch_idx}, global step {pl_module.global_step}")
         check_idx = batch_idx if self.log_on_batch_idx else pl_module.global_step
-        if not self.disabled and (pl_module.global_step > 0 or self.log_first_step) and self.check_frequency(check_idx):
-            print(f"Logging training images in batch {batch_idx} at step {pl_module.global_step}")
+        if (
+            not self.disabled
+            and (pl_module.global_step > 0 or self.log_first_step)
+            and self.check_frequency(check_idx)
+        ):
+            print(
+                f"Logging training images in batch {batch_idx} at step {pl_module.global_step}"
+            )
             self.log_img(pl_module, batch, batch_idx, split="train")
 
     @rank_zero_only
-    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+    def on_validation_batch_end(
+        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
+    ):
         # print(f"Process {os.getpid()} in on_validation_batch_end(), batch_idx {batch_idx}, global step {pl_module.global_step}")
-        if self.log_validation and batch_idx % self.val_batch_frequency == 0 and not self.disabled and pl_module.global_step > 0:
+        if (
+            self.log_validation
+            and batch_idx % self.val_batch_frequency == 0
+            and not self.disabled
+            and pl_module.global_step > 0
+        ):
             print(f"Logging validation images in batch {batch_idx}")
             self.log_img(pl_module, batch, batch_idx, split="val")
-        if hasattr(pl_module, 'calibrate_grad_norm'):
-            if (pl_module.calibrate_grad_norm and batch_idx % 25 == 0) and batch_idx > 0:
+        if hasattr(pl_module, "calibrate_grad_norm"):
+            if (
+                pl_module.calibrate_grad_norm and batch_idx % 25 == 0
+            ) and batch_idx > 0:
                 self.log_gradients(trainer, pl_module, batch_idx=batch_idx)
 
     @rank_zero_only
     def on_train_epoch_end(self, trainer, pl_module):
         # print(f"Process {os.getpid()} in on_train_epoch_end(), global step {pl_module.global_step}")
-        #print(self.metrics["train"].items())
+        # print(self.metrics["train"].items())
         for metric_name, metric_values in self.metrics["train"].items():
             self.metrics["train"][metric_name] = np.concatenate(metric_values, axis=0)
 
-        wandb.log({"train/mse": np.mean(self.metrics["train"]["mse"]), "train/ssim": np.mean(self.metrics["train"]["ssim"]), "train/pcc": np.mean(self.metrics["train"]["pcc"]), "train/cdist": np.mean(self.metrics["train"]["cdist"]), "train/edist": np.mean(self.metrics["train"]["edist"])}, step=pl_module.global_step)
+        wandb.log(
+            {
+                "train/mse": np.mean(self.metrics["train"]["mse"]),
+                "train/ssim": np.mean(self.metrics["train"]["ssim"]),
+                "train/pcc": np.mean(self.metrics["train"]["pcc"]),
+                "train/cdist": np.mean(self.metrics["train"]["cdist"]),
+                "train/edist": np.mean(self.metrics["train"]["edist"]),
+            },
+            step=pl_module.global_step,
+        )
         self.metrics["train"] = defaultdict(list)
 
     @rank_zero_only
     def on_validation_epoch_end(self, trainer, pl_module):
-        print(f"Process {os.getpid()} in on_validation_epoch_end(), global step {pl_module.global_step}")
-        if self.log_to_slack and self.monitor_val_metric and self.monitor_val_metric in trainer.logged_metrics:
+        print(
+            f"Process {os.getpid()} in on_validation_epoch_end(), global step {pl_module.global_step}"
+        )
+        if (
+            self.log_to_slack
+            and self.monitor_val_metric
+            and self.monitor_val_metric in trainer.logged_metrics
+        ):
             val = trainer.logged_metrics[self.monitor_val_metric]
             if self._last_val_loss:
                 if self._last_val_loss > val:
                     self._last_val_loss = val
-                    send_message_to_slack(f"🎉 Validation metric updated: {self.monitor_val_metric} reached {val}")
             else:
                 self._last_val_loss = val
         for metric_name, metric_values in self.metrics["val"].items():
             self.metrics["val"][metric_name] = np.concatenate(metric_values, axis=0)
-        
-        wandb.log({"val/mse": np.mean(self.metrics["val"]["mse"]), "val/ssim": np.mean(self.metrics["val"]["ssim"]), "val/pcc": np.mean(self.metrics["val"]["pcc"]), "val/cdist": np.mean(self.metrics["val"]["cdist"]), "val/edist": np.mean(self.metrics["val"]["edist"])}, step=pl_module.global_step)
+
+        wandb.log(
+            {
+                "val/mse": np.mean(self.metrics["val"]["mse"]),
+                "val/ssim": np.mean(self.metrics["val"]["ssim"]),
+                "val/pcc": np.mean(self.metrics["val"]["pcc"]),
+                "val/cdist": np.mean(self.metrics["val"]["cdist"]),
+                "val/edist": np.mean(self.metrics["val"]["edist"]),
+            },
+            step=pl_module.global_step,
+        )
         self.metrics["val"] = defaultdict(list)
 
 
@@ -266,12 +352,12 @@ class CUDACallback(Callback):
 def get_mem_info(pid: int) -> dict[str, int]:
     res = defaultdict(int)
     for mmap in psutil.Process(pid).memory_maps():
-        res['rss'] += mmap.rss
-        res['pss'] += mmap.pss
-        res['uss'] += mmap.private_clean + mmap.private_dirty
-        res['shared'] += mmap.shared_clean + mmap.shared_dirty
-    if mmap.path.startswith('/'):
-        res['shared_file'] += mmap.shared_clean + mmap.shared_dirty
+        res["rss"] += mmap.rss
+        res["pss"] += mmap.pss
+        res["uss"] += mmap.private_clean + mmap.private_dirty
+        res["shared"] += mmap.shared_clean + mmap.shared_dirty
+    if mmap.path.startswith("/"):
+        res["shared_file"] += mmap.shared_clean + mmap.shared_dirty
     return res
 
 
@@ -312,20 +398,20 @@ class CPUMemoryMonitor(Callback):
 
     @staticmethod
     def format(size: int) -> str:
-        for unit in ('', 'K', 'M', 'G'):
+        for unit in ("", "K", "M", "G"):
             if size < 1024:
                 break
             size /= 1024.0
         return "%.1f%s" % (size, unit)
-  
-    def on_train_epoch_start(self, trainer, pl_module):
+
+    def on_train_epoch_start(self):
         print("Start of training epoch", self.str())
 
-    def on_train_epoch_end(self, trainer, pl_module):
+    def on_train_epoch_end(self):
         print("End of training epoch", self.str())
 
-    def on_validation_epoch_start(self, trainer, pl_module):
+    def on_validation_epoch_start(self):
         print("Start of validation epoch", self.str())
 
-    def on_validation_epoch_end(self, trainer, pl_module):
+    def on_validation_epoch_end(self):
         print("End of validation epoch", self.str())
